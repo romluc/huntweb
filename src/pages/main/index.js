0131033ee3b780131f52351e4757e69077e30dd1
romluc <romulofilhofilho@gmail.com>
2019-08-28 15:45:30 -0300

    Initial commit

---
+++ src/pages/main/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import './styles.css';
+
+export default class Main extends Component {
+	state = {
+		products: []
+	};
+
+	componentDidMount() {
+		this.loadProducts();
+	}
+
+	loadProducts = async () => {
+		const response = await api.get('/products');
+		this.setState({ products: response.data.docs });
+		console.log(response.data);
+	};
+
+	prevPage = () => {};
+
+	nextPage = () => {};
+
+	render() {
+		const { products } = this.state;
+		return (
+			<div className='product-list'>
+				{products.map(product => (
+					<article key={product._id}>
+						<strong>{product.title}</strong>
+						<p>{product.description}</p>
+						<a href='#'>Acessar</a>
+					</article>
+				))}
+				<div className='actions'>
+					<button onClick={this.prevPage}>Anterior</button>
+					<button onClick={this.nextPage}>Próxima</button>
+				</div>
+			</div>
+		);
+	}
+}
